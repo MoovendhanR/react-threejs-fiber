d6eaff218456a8d44d2a403089ec7b4febdfd789
MoovendhanR <moovendhanr1011@gmail.com>
2023-06-15 12:00:58 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Bmw from "./Components/Bmw";
+function App() {
+
+  return (
+  <>
+  <Bmw/>
+  </>
+  );
+}
+
+export default App;
---
+++ src/Components/Bmw.jsx
@@ -0,0 +1,26 @@
+import { PresentationControls, Stage, useGLTF } from "@react-three/drei";
+import { Canvas } from "@react-three/fiber";
+
+
+function Model(props){
+   const {scene} = useGLTF("/bmw.glb");
+
+   return <primitive object={scene} {...props}/>
+}
+
+function Bmw() {
+
+  return (
+  <Canvas dpr={[1,2]} shadows camera={{fav:45}} style={{"position":"absolute"}}>
+    <color attach="background" args={["#101010"]}/>
+    <PresentationControls speed={1.5} global zoom={.5} polar={[-0.1,Math.PI/4]}>
+      <Stage environment={null}>
+           <Model scale={0.01}/>
+      </Stage>
+
+    </PresentationControls>
+  </Canvas>
+  );
+}
+
+export default Bmw;
